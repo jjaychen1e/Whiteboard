1c3230332c10fcad0fecc2d610721297883133f6
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2021-11-21 21:27:55 +0800

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,6 +4,7 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.http.server.configuration.port = 8181
 
     // register routes
     try routes(app)
